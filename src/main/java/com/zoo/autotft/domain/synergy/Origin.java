083a4e7792b7fb57516f3acff3f22f78e3839b59
1000zoo <cjswldn99@gmail.com>
2024-01-22 10:42:56 +0900

    refactor: 중복되는 equals, hashCode 메서드를 상위 추상 클래스로 이동

--- src/main/java/com/zoo/autotft/domain/synergy/Origin.java
+++ src/main/java/com/zoo/autotft/domain/synergy/Origin.java
@@ -1,27 +1,8 @@
 package com.zoo.autotft.domain.synergy;
 
-import java.util.Objects;
-
 public class Origin extends Synergy {
 
     public Origin(String name, ActivateConditions conditions) {
         super(name, conditions);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Synergy synergy = (Synergy) o;
-        return Objects.equals(name, synergy.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
--- src/main/java/com/zoo/autotft/domain/synergy/Synergy.java
+++ src/main/java/com/zoo/autotft/domain/synergy/Synergy.java
@@ -2,6 +2,7 @@ package com.zoo.autotft.domain.synergy;
 
 import com.zoo.autotft.domain.Champion;
 import java.util.List;
+import java.util.Objects;
 import lombok.Getter;
 
 @Getter
@@ -31,5 +32,22 @@ public abstract class Synergy {
 
     public int getActivatedState(int count) {
         return conditions.getActivatedState(count);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Synergy synergy = (Synergy) o;
+        return Objects.equals(name, synergy.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
