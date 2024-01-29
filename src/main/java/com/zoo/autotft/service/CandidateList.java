572f033cb94bcb10635c8e8672155d8aa4e991f4
1000zoo <cjswldn99@gmail.com>
2024-01-29 23:27:22 +0900

    feat: 가능한 조합들을 좋은 순서대로 최대 10개만 가지도록 관리하는 클래스

---
+++ src/main/java/com/zoo/autotft/service/CandidateList.java
@@ -0,0 +1,26 @@
+package com.zoo.autotft.service;
+
+import com.zoo.autotft.domain.Deck;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class CandidateList {
+
+    private final static int MAX_SIZE = 10;
+
+    private final Queue<Deck> candidateList = new PriorityQueue<>(Comparator.comparingInt(Deck::value));
+
+    public void add(Deck deck) {
+        if (candidateList.size() >= MAX_SIZE) {
+            candidateList.poll();
+        }
+        candidateList.add(deck);
+    }
+
+    public List<Deck> getResults() {
+        return new ArrayList<>(candidateList);
+    }
+}
