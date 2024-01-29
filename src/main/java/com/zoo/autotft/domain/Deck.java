50a0841b1482747d80d3151cc4bb05cfddf7400b
1000zoo <cjswldn99@gmail.com>
2024-01-29 23:03:37 +0900

    feat: 조합을 관리할 클래스 Deck 구현

---
+++ src/main/java/com/zoo/autotft/domain/Deck.java
@@ -0,0 +1,55 @@
+package com.zoo.autotft.domain;
+
+import com.zoo.autotft.domain.synergy.Synergy;
+import com.zoo.autotft.domain.synergy.SynergyStatus;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+
+    private final int level;
+    private final List<Champion> champions;
+    private final SynergyStatus status;
+
+    public Deck(int level, List<Champion> champions, List<Synergy> status) {
+        validate(level, champions);
+        this.level = level;
+        this.champions = new ArrayList<>(champions);
+        this.status = SynergyStatus.of(status);
+        setting();
+    }
+
+    public Deck(Deck forCopy) {
+        level = forCopy.level;
+        champions = new ArrayList<>(forCopy.champions);
+        status = new SynergyStatus(forCopy.status);
+    }
+
+    private void setting() {
+        champions.forEach(champion -> champion.getSynergies().forEach(status::put));
+    }
+
+    private void validate(int level, List<Champion> champions) {
+        if (level < champions.size()) {
+            throw new IllegalStateException("이미 많은 챔피언들이 속해있습니다.");
+        }
+    }
+
+    public void add(Champion champion) {
+        champions.add(champion);
+        champion.getSynergies().forEach(status::put);
+    }
+
+    public int size() {
+        return champions.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Deck{" +
+                "level=" + level +
+                ", champions=" + champions +
+                ", status=" + status +
+                '}';
+    }
+}
---
+++ src/test/java/com/zoo/autotft/domain/DeckTest.java
@@ -0,0 +1,65 @@
+package com.zoo.autotft.domain;
+
+import com.zoo.autotft.domain.synergy.Synergy;
+import com.zoo.autotft.repository.JsonRepositoryController;
+import com.zoo.autotft.repository.Repository;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+
+    private static Repository<Champion> championRepository;
+    private static Repository<Synergy> synergyRepository;
+
+    @BeforeEach
+    void setUp() {
+        championRepository = JsonRepositoryController.getChampionRepository();
+        synergyRepository = JsonRepositoryController.getSynergyRepository();
+    }
+
+    @Test
+    @DisplayName("Deck이 잘 만들어지는 지 테스트")
+    void createDeckTest() {
+        // given
+        int level = 10;
+        List<Champion> champions = championOf(List.of("나미", "타릭", "케넨"));
+        List<Synergy> synergies = synergyOf(List.of("디스코"));
+
+        // when
+        Deck deck = new Deck(level, champions, synergies);
+
+        // then
+        System.out.println(deck);
+
+    }
+
+    @Test
+    @DisplayName("Deck copy 테스트")
+    void copyTest() {
+        // given
+        int level = 10;
+        List<Champion> champions = championOf(List.of("나미", "타릭", "케넨"));
+        List<Synergy> synergies = synergyOf(List.of("디스코"));
+        Deck origin = new Deck(level, champions, synergies);
+
+        // when
+        Deck copy = new Deck(origin);
+        origin.add(championRepository.findByName("루시안"));
+
+        // then
+        System.out.println("origin => " + origin);
+        System.out.println("copy => " + copy);
+
+    }
+
+    private List<Synergy> synergyOf(List<String> names) {
+        return names.stream().map(synergyRepository::findByName).collect(Collectors.toList());
+    }
+
+    private List<Champion> championOf(List<String> names) {
+        return names.stream().map(championRepository::findByName).collect(Collectors.toList());
+    }
+}
